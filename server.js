5339400817eade35578c78be617b6dbb977a980d
Yusuf Code <yusuf1code@gmail.com>
2021-11-15 06:00:47 +0300

    port works on env

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const next = require('next')
+require('dotenv/config')
 const dev = process.env.NODE_ENV !== 'production'
 
-const app = next({dev})
+const app = next({ dev })
 const handle = app.getRequestHandler()
-const port = 3000
+const port = process.env.SERVER_PORT || 3000
 
 app.prepare().then(()=>{
   const server = express()
-  server.get('*', (req, res)=>{
-    return handle(req,res)
-  })
+
+  server.get('*', (req, res) => handle(req, res))
+
   server.listen(port, (err) => {
     if(err) throw err
-    console.log(`SERVER SUCCESS: ${port}`)
+    console.log(`Server listen on http://127.0.0.1:${port}`)
   })
-}).catch((e)=>{
-  console.log(e.stack)
-  process.exit(1)
+
 })
