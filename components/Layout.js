0c18102bc32a9261f88b7fc0fb8ffdeedf7216b9
Yusuf Code <yusuf1code@gmail.com>
2021-12-30 20:08:01 +0300

    google adsense script added

--- components/Layout.js
+++ components/Layout.js
@@ -8,20 +8,19 @@ export default function Layout({ children }) {
       <Head>
         <link rel="icon" href="/img/logo/logo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732757580085068" crossorigin="anonymous"></script>
       </Head>
       {/* <>
-      <Script strategy="lazyOnLoad" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732757580085068"/>
+        <Script strategy="lazyOnLoad" src="https://www.googletagmanager.com/gtag/js?id=G-E4S3SMK9KN"/>
+        <Script strategy="lazyOnLoad">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-      <Script strategy="lazyOnLoad" src="https://www.googletagmanager.com/gtag/js?id=G-E4S3SMK9KN"/>
-      <Script strategy="lazyOnLoad">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-E4S3SMK9KN');
-        `}
-      </Script>
+          gtag('config', 'G-E4S3SMK9KN');
+          `}
+        </Script>
       </> */}
       <Navbar />
       <div className="mainContainer">
