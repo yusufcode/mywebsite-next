bf19a9c89b3747da5d0e819fcc6c2b6b8d8a6f99
Yusuf Code <yusuf1code@gmail.com>
2021-12-30 21:54:14 +0300

    google adsense script added

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,8 @@ export default function Layout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
       </Head>
 
+      <Script id="gad" strategy="lazyOnload" src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9732757580085068`}/>
+
       <Script id="ga1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-E4S3SMK9KN`}/>
       <Script id="ga2" strategy="lazyOnload">
         {`
