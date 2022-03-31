1b73fc32a1f05523974538ff3e184d2f3743bff3
yusufcode <75755242+yusufcode@users.noreply.github.com>
2022-03-31 11:44:18 +0300

    Update cv-resume.js

--- pages/cv-resume.js
+++ pages/cv-resume.js
@@ -50,7 +50,6 @@ export default function Home() {
         <div className={styles.sectionBody}>
           <TitleText title="Full Name" text="Yusuf Akbaba" />
           <TitleText title="Age" text="22" />
-          <TitleText title="Location" text="St. Petersburg, Russia" />
         </div>
       </div>
 
