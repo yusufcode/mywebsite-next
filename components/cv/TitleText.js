8da30e7ff5a775ea9c33e57228910142b580ad16
yusufcode <75755242+yusufcode@users.noreply.github.com>
2022-04-26 01:30:37 +0300

    Update TitleText.js

--- components/cv/TitleText.js
+++ components/cv/TitleText.js
@@ -1,19 +1,11 @@
 import styles from '../../styles/cv/TitleText.module.scss'
 
-export default function TitleText({title, text, linkMail, linkTel, linkLinkedin}) {    
+export default function TitleText({title, text, link}) {    
   return (
     <>
-    {linkMail ?
+    {link ?
       <p className={styles.titleText}>
-          <span className={styles.title}>{title}: </span><a href={`mailto:${linkMail}`} className={styles.link}>{text}</a>
-      </p>
-      : linkTel ?
-      <p className={styles.titleText}>
-          <span className={styles.title}>{title}: </span><a href={`tel:${linkTel}`} className={styles.link}>{text}</a>
-      </p>
-      : linkLinkedin ?
-      <p className={styles.titleText}>
-          <span className={styles.title}>{title}: </span><a href={`www.linkedin.com/in/${linkLinkedin}`} className={styles.link}>{text}</a>
+          <span className={styles.title}>{title}: </span><a href={link} className={styles.link}>{text}</a>
       </p>
       : 
       <p className={styles.titleText}>
