c4e1654c53369960e17176f7c440b65789400ae0
yusufcode <75755242+yusufcode@users.noreply.github.com>
2022-04-26 01:26:02 +0300

    Update TitleText.js

--- components/cv/TitleText.js
+++ components/cv/TitleText.js
@@ -1,15 +1,19 @@
 import styles from '../../styles/cv/TitleText.module.scss'
 
-export default function TitleText({title, text, linkMail, linkTel}) {    
+export default function TitleText({title, text, linkMail, linkTel, linkLinkedin}) {    
   return (
     <>
     {linkMail ?
       <p className={styles.titleText}>
-          <span className={styles.title}>{title}: </span><a href={`mailto:${text}`} className={styles.link}>{text}</a>
+          <span className={styles.title}>{title}: </span><a href={`mailto:${linkMail}`} className={styles.link}>{text}</a>
       </p>
       : linkTel ?
       <p className={styles.titleText}>
-          <span className={styles.title}>{title}: </span><a href={`tel:${text}`} className={styles.link}>{text}</a>
+          <span className={styles.title}>{title}: </span><a href={`tel:${linkTel}`} className={styles.link}>{text}</a>
+      </p>
+      : linkLinkedin ?
+      <p className={styles.titleText}>
+          <span className={styles.title}>{title}: </span><a href={`www.linkedin.com/in/${linkLinkedin}`} className={styles.link}>{text}</a>
       </p>
       : 
       <p className={styles.titleText}>
